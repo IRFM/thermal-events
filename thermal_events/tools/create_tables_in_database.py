2876f2e7e8d8aa2c83ed002ccf6599fff10aa901
GRELIER Erwan 264877 <erwan.grelier@cea.fr>
2023-07-19 15:18:59 +0200

    refactor: :recycle: Use a compatibility matrix between lines of sight and thermal events types

--- thermal_events/tools/create_tables_in_database.py
+++ thermal_events/tools/create_tables_in_database.py
@@ -15,17 +15,6 @@ with get_db() as session:
     # Create the tables
     Base.metadata.create_all(session.bind)
 
-    # Create content of table lines_of_sight
-    session.add_all(
-        [
-            LineOfSight(name=name)
-            for name in [
-                "line of sight 1",
-                "line of sight 2",
-            ]
-        ]
-    )
-
     # Create content of table users
     session.add_all(
         [
@@ -37,31 +26,40 @@ with get_db() as session:
         ]
     )
 
+    # Create content of table lines_of_sight
+    lines_of_sight = [
+        "line of sight 1",
+        "line of sight 2",
+    ]
+    session.add_all([LineOfSight(name=name) for name in lines_of_sight])
+
     # Create content of table thermal_event_types
-    session.add_all(
-        [
-            ThermalEventType(name=name)
-            for name in [
-                "thermal event 1",
-                "thermal event 2",
-            ]
-        ]
-    )
+    thermal_event_types = [
+        "thermal event 1",
+        "thermal event 2",
+        "thermal event 3",
+    ]
+    session.add_all([ThermalEventType(name=name) for name in thermal_event_types])
+
+    # Compatibility matrix (line of sight, thermal event type)
+    compat = [
+        [1, 1, 0],
+        [1, 0, 1],
+    ]
 
     # Create content of table thermal_event_type_lines_of_sight
-    session.add_all(
-        [
-            ThermalEventTypeLineOfSight(
-                thermal_event_type="thermal event 1", line_of_sight="line of sight 1"
-            ),
-            ThermalEventTypeLineOfSight(
-                thermal_event_type="thermal event 2", line_of_sight="line of sight 1"
-            ),
-            ThermalEventTypeLineOfSight(
-                thermal_event_type="thermal event 1", line_of_sight="line of sight 2"
-            ),
-        ]
-    )
+    thermal_event_type_lines_of_sight = []
+    for ind_los, compat_los in enumerate(compat):
+        for ind_type, flag in enumerate(compat_los):
+            if not flag:
+                continue
+            thermal_event_type_lines_of_sight.append(
+                ThermalEventTypeLineOfSight(
+                    thermal_event_type=thermal_event_types[ind_type],
+                    line_of_sight=lines_of_sight[ind_los],
+                ),
+            )
+    session.add_all(thermal_event_type_lines_of_sight)
 
     # Create content of table datasets
     session.add_all(
@@ -85,19 +83,23 @@ with get_db() as session:
             ),
             AnalysisStatus(
                 name="Analyzed (OK)",
-                description="A thermal event that has been analyzed, and does not need follow-up analysis",
+                description="A thermal event that has been analyzed, and does "
+                + "not need follow-up analysis",
             ),
             AnalysisStatus(
                 name="Analyzed (follow-up required)",
-                description="A thermal event that has been analyzed, but which requires follow-up analysis",
+                description="A thermal event that has been analyzed, but which "
+                + "requires follow-up analysis",
             ),
             AnalysisStatus(
                 name="Detection Error",
-                description="A false positive, a detection which is not a thermal event",
+                description="A false positive, a detection which is not a "
+                + "thermal event",
             ),
             AnalysisStatus(
                 name="Detection Problem",
-                description="A thermal event which is detected, but not properly encompassed, classified and/or tracked",
+                description="A thermal event which is detected, but not properly "
+                + "encompassed, classified and/or tracked",
             ),
         ]
     )
